87a521160431517386310fe21de5f69ead4709e5
Ashok <139360747+Ashok-A15@users.noreply.github.com>
2024-06-24 22:05:41 +0530

    Update 6.quicksort.c

--- 6.quicksort.c
+++ 6.quicksort.c
@@ -1,3 +1,4 @@
+
 /* program to implement quick sort*/
 #include<stdio.h>
 #include<stdlib.h>
@@ -73,4 +74,70 @@ while(n<1034)
  fclose(f1);
  fclose(f2);
  fclose(f3);
+}
+
+
+
+
+
+
+TESTER 
+/* program to implement quick sort*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+ int count;
+void swap(int *a,int * b)
+{
+ int temp=*a;
+ *a=*b;
+ *b=temp;
+}
+int partition(int * arr,int beg,int end)
+{
+ int pivot =arr[beg];
+ int i=beg,j=end+1;
+do{
+ do{
+ count++;
+ i++;
+ }while(arr[i]<pivot);
+ do{
+ count++;
+ j--;
+ }while(arr[j]>pivot);
+ swap(&arr[i],&arr[j]);
+ }while(i<j);
+ swap(&arr[i],&arr[j]);
+swap(&arr[beg],&arr[j]);
+return j;
+}
+void quicksort(int *arr,int beg,int end)
+{
+if(beg<end)
+{
+ int split=partition(arr,beg,end);
+ quicksort(arr,beg,split-1);
+ quicksort(arr,split+1,end);
+}
+}
+void main()
+{
+ int *arr, n;
+ printf("ENTER THE NUMBER OF ELEMENTS\n");
+ scanf("%d",&n);
+arr=(int *)malloc(sizeof(int)*n);
+printf("ENTER THE ELEMENTS OF THE ARRAY\n");
+for(int i=0;i<n;i++)
+ scanf("%d",&arr[i]);
+printf("THE ELEMENTS OF THE ARRAY BEFORE SORTING\n"); 
+for(int i=0;i<n;i++)
+printf("%d ",arr[i]);
+printf("\n");
+quicksort(arr,0,n-1);
+printf("THE ELEMENTS OF THE ARRAY BEFORE SORTING\n"); 
+for(int i=0;i<n;i++)
+printf("%d ",arr[i]);
+ printf("\n");
+ printf("\n");
 }
